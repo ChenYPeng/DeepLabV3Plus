5e36a01dddc92a419dc9e8d0e54851cbee5f7e0b
SmileAltas <57321786+ChenYPeng@users.noreply.github.com>
2022-07-26 10:10:56 +0800

    Update and rename predict to predict.py

---
+++ predict.py
@@ -0,0 +1,83 @@
+import os
+import json
+import tqdm
+import torch
+import glob
+
+import numpy as np
+import torchvision.transforms as T
+
+from PIL import Image
+from gpuinfo import GPUInfo
+
+from core.log import get_logger
+from dataloaders.utils import decode_segmap
+from models import build_model, modified_mode_dict
+
+np.seterr(divide='ignore', invalid='ignore')
+
+
+def test(cfg, runid, use_pth='best_Eval_MIoU.pth'):
+    dataset = cfg["dataset"]
+    model_name = cfg["model_name"]
+    class_num = cfg['num_class']
+    train_logdir = os.path.join('run', dataset, model_name, runid)
+    test_logdir = os.path.join('res', dataset, model_name, runid)
+    logger = get_logger(test_logdir, 'test')
+    logger.info(f'Conf | use logdir {train_logdir}')
+    logger.info(f'Conf | use dataset {dataset}')
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # 加载测试集
+    image_files = []
+    input_path = 'samples/src'
+    output_path = 'samples/'
+    if os.path.isdir(input_path):
+        for ext in ['png', 'jpeg', 'jpg', 'JPEG', 'bmp']:
+            files = glob.glob(os.path.join(input_path, '**/*.%s' % ext), recursive=True)
+            if len(files) > 0:
+                image_files.extend(files)
+    elif os.path.isfile(input_path):
+        image_files.append(input_path)
+
+    transform = T.Compose([
+        T.ToTensor(),
+        T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ])
+
+    model = build_model(cfg).to(device)
+    gpu_ids = GPUInfo.check_empty()
+
+    # 多GPU测试
+    if len(gpu_ids) > 1:
+        new_state_dict = modified_mode_dict(os.path.join(train_logdir, use_pth))
+        model.load_state_dict(new_state_dict)
+    # 单GPU测试
+    else:
+        model.load_state_dict(torch.load(os.path.join(train_logdir, use_pth)))
+
+    model = model.eval()
+
+    for img_path in tqdm.tqdm(image_files):
+        ext = os.path.basename(img_path).split('.')[-1]
+        img_name = os.path.basename(img_path)[:-len(ext) - 1]
+        img = Image.open(img_path).convert('RGB')
+
+        img = transform(img).unsqueeze(0)  # To tensor of NCHW
+        img = img.to(device)
+        with torch.no_grad():
+            outputs = model(img)
+        pred = outputs.max(1)[1].squeeze().cpu().data.numpy()  # (1024, 1280)
+        pred = decode_segmap(pred, dataset, plot=False)
+        pred = Image.fromarray(np.unit8(pred * 255))
+        pred.save(os.path.join(output_path, img_name + '.png'))
+
+
+if __name__ == '__main__':
+    config = 'hanfeng/deeplab-resnet-hanfeng.json'
+    run_id = '2022-07-25-16-59-9929'
+    config_dir = os.path.join('configs', config)
+    with open(config_dir, 'r') as fp:
+        cfg = json.load(fp)
+
+    test(cfg, run_id)
